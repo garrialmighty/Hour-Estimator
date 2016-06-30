f01059f73bd2f79c4a7445cac314627798fec811
Garri Adrian Nablo <garri.almighty@gmail.com>
2016-06-30 20:33:22 +0800

    Implemented TaskViewController

--- Hours Estimator/TasksViewController.swift
+++ Hours Estimator/TasksViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 final class TasksViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let viewModel = [["Planning": ["Inception Deck", "Market Research", "Stakeholder Interview"]],
+                     ["Design": ["Wireframing", "Mockups", "Flow User Testing", "Style Guide", "Brand Analysis"]],
+                     ["Development": ["Setup and Scaffolding"]]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,37 @@ final class TasksViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func didTapNext(sender: AnyObject) {
+        print(self.tableView.indexPathsForSelectedRows)
+    }
+}
 
+extension TasksViewController: UITableViewDataSource {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return self.viewModel.count
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let tasks = self.viewModel[section].values.first else { return 0 }
+        return tasks.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCellWithIdentifier("TaskCell", forIndexPath: indexPath) as? TaskTableViewCell,
+            let tasks = self.viewModel[indexPath.section].values.first
+            else { return TaskTableViewCell() }
+        
+        cell.taskLabel.text = tasks[indexPath.item]
+        
+        return cell
+    }
+}
 
+extension TasksViewController: UITableViewDelegate {
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.viewModel[section].keys.first
+    }
 }
 
