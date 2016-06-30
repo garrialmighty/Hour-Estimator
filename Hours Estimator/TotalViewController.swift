24e0712204874c5dcac36bb436c11f357a59b8df
Garri Adrian Nablo <garri.almighty@gmail.com>
2016-07-01 01:09:46 +0800

    Implemented TotalViewController

--- Hours Estimator/TotalViewController.swift
+++ Hours Estimator/TotalViewController.swift
@@ -8,28 +8,110 @@
 
 import UIKit
 
-class TotalViewController: UIViewController {
+final class TotalViewController: UIViewController {
 
+    private let totalHours: Double!
+    private let totalPrice: Double!
+    
+    init(totalHours: Double, totalPrice: Double) {
+        self.totalHours = totalHours
+        self.totalPrice = totalPrice
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let hoursContentView = UIView()
+        hoursContentView.translatesAutoresizingMaskIntoConstraints = false
+        hoursContentView.backgroundColor = UIColor(red: 254/255, green: 151/255, blue: 44/255, alpha: 1.0)
+        self.view.addSubview(hoursContentView)
+        NSLayoutConstraint.activateConstraints([
+            hoursContentView.topAnchor.constraintEqualToAnchor(self.view.topAnchor),
+            hoursContentView.leadingAnchor.constraintEqualToAnchor(self.view.leadingAnchor),
+            hoursContentView.trailingAnchor.constraintEqualToAnchor(self.view.trailingAnchor)
+            ])
+        
+        let clockImageView = UIImageView(image: UIImage(named: "clock"))
+        clockImageView.translatesAutoresizingMaskIntoConstraints = false
+        hoursContentView.addSubview(clockImageView)
+        NSLayoutConstraint.activateConstraints([
+            clockImageView.centerYAnchor.constraintEqualToAnchor(hoursContentView.centerYAnchor),
+            clockImageView.leadingAnchor.constraintEqualToAnchor(hoursContentView.leadingAnchor, constant: 30.0)
+            ])
+        
+        let totalHoursLabel = UILabel()
+        totalHoursLabel.text = "\(totalHours)"
+        totalHoursLabel.textColor = .whiteColor()
+        totalHoursLabel.font = UIFont.boldSystemFontOfSize(30.0)
+        totalHoursLabel.translatesAutoresizingMaskIntoConstraints = false
+        hoursContentView.addSubview(totalHoursLabel)
+        NSLayoutConstraint.activateConstraints([
+            totalHoursLabel.centerYAnchor.constraintEqualToAnchor(clockImageView.centerYAnchor),
+            totalHoursLabel.leadingAnchor.constraintEqualToAnchor(clockImageView.trailingAnchor, constant: 20.0)
+            ])
+        
+        let hoursLabel = UILabel()
+        hoursLabel.text = "TOTAL HOURS"
+        hoursLabel.textColor = .whiteColor()
+        hoursLabel.translatesAutoresizingMaskIntoConstraints = false
+        hoursContentView.addSubview(hoursLabel)
+        NSLayoutConstraint.activateConstraints([
+            hoursLabel.leadingAnchor.constraintEqualToAnchor(totalHoursLabel.leadingAnchor),
+            hoursLabel.bottomAnchor.constraintEqualToAnchor(totalHoursLabel.topAnchor, constant: -10.0)
+            ])
+        
+        let priceContentView = UIView()
+        priceContentView.translatesAutoresizingMaskIntoConstraints = false
+        priceContentView.backgroundColor = UIColor(red: 0.0, green: 165/255, blue: 96/255, alpha: 1.0)
+        self.view.addSubview(priceContentView)
+        NSLayoutConstraint.activateConstraints([
+            priceContentView.leadingAnchor.constraintEqualToAnchor(self.view.leadingAnchor),
+            priceContentView.trailingAnchor.constraintEqualToAnchor(self.view.trailingAnchor),
+            priceContentView.topAnchor.constraintEqualToAnchor(hoursContentView.bottomAnchor),
+            priceContentView.bottomAnchor.constraintEqualToAnchor(self.view.bottomAnchor)
+            ])
+        
+        let moneyImageView = UIImageView(image: UIImage(named: "money"))
+        moneyImageView.translatesAutoresizingMaskIntoConstraints = false
+        priceContentView.addSubview(moneyImageView)
+        NSLayoutConstraint.activateConstraints([
+            moneyImageView.centerYAnchor.constraintEqualToAnchor(priceContentView.centerYAnchor),
+            moneyImageView.leadingAnchor.constraintEqualToAnchor(priceContentView.leadingAnchor, constant: 30.0)
+            ])
+        
+        let totalPriceLabel = UILabel()
+        totalPriceLabel.text = "$\(totalPrice)"
+        totalPriceLabel.textColor = .whiteColor()
+        totalPriceLabel.font = .boldSystemFontOfSize(30.0)
+        totalPriceLabel.translatesAutoresizingMaskIntoConstraints = false
+        priceContentView.addSubview(totalPriceLabel)
+        NSLayoutConstraint.activateConstraints([
+            totalPriceLabel.centerYAnchor.constraintEqualToAnchor(priceContentView.centerYAnchor),
+            totalPriceLabel.leadingAnchor.constraintEqualToAnchor(moneyImageView.trailingAnchor, constant: 20.0)
+            ])
+        
+        let priceLabel = UILabel()
+        priceLabel.text = "ESTIMATED PRICE"
+        priceLabel.textColor = .whiteColor()
+        priceLabel.translatesAutoresizingMaskIntoConstraints = false
+        priceContentView.addSubview(priceLabel)
+        NSLayoutConstraint.activateConstraints([
+            priceLabel.leadingAnchor.constraintEqualToAnchor(totalPriceLabel.leadingAnchor),
+            priceLabel.bottomAnchor.constraintEqualToAnchor(totalPriceLabel.topAnchor, constant: -10.0)
+            ])
+        
+        // make both content views have the same height
+        hoursContentView.heightAnchor.constraintEqualToAnchor(priceContentView.heightAnchor).active = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
