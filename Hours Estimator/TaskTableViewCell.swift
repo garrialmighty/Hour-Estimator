50b7ef1677fdfb419d945e8f97e67d732093c494
Garri Adrian Nablo <garri.almighty@gmail.com>
2016-06-30 17:28:40 +0800

    Added TaskTableViewCell to the project

---
+++ Hours Estimator/TaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewCell.swift
+//  Hours Estimator
+//
+//  Created by Garri Adrian Nablo on 6/30/16.
+//  Copyright © 2016 Knack Studios. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
