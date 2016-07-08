6538cdc2f492ffbc2645bad67afeae6eb2f720e0
Garri Adrian Nablo <garri.almighty@gmail.com>
2016-07-08 10:59:36 +0800

    Refactored TaskTableViewCell to implement new design

--- Hours Estimator/TaskTableViewCell.swift
+++ Hours Estimator/TaskTableViewCell.swift
@@ -11,23 +11,18 @@ import FontAwesome_swift
 
 final class TaskTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var checkboxImageView: UIImageView!
     @IBOutlet weak var taskLabel: UILabel!
-    @IBOutlet weak var arrowLabel: UILabel!
-    @IBOutlet weak var addToEstimateView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        self.arrowLabel.font = .fontAwesomeOfSize(20)
-        self.arrowLabel.text = .fontAwesomeIconWithCode("fa-arrow-circle-right")
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
-        self.contentView.backgroundColor = selected ? UIColor(red: 232/255, green: 68/255, blue: 59/255, alpha: 1.0) : UIColor(red: 0.0, green: 196/255, blue: 237/255, alpha: 1.0)
-        self.addToEstimateView.backgroundColor = selected ? UIColor(red: 209/255, green: 61/255, blue: 53/255, alpha: 1.0) : UIColor(red: 0.0, green: 177/255, blue: 214/255, alpha: 1.0)
+        self.checkboxImageView.image = UIImage(named: selected ? "checkboxSelected" : "checkboxDeselected")
     }
-
 }
