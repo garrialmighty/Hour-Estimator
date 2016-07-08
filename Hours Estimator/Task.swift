fac10091b9f233ace32e9b27324ab1ec7c2929a9
Garri Adrian Nablo <garri.almighty@gmail.com>
2016-07-08 15:20:56 +0800

    Added Task Realm model

---
+++ Hours Estimator/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  Hours Estimator
+//
+//  Created by Garri Adrian Nablo on 7/8/16.
+//  Copyright © 2016 Knack Studios. All rights reserved.
+//
+
+import UIKit
+
+class Task: Object {
+
+}
