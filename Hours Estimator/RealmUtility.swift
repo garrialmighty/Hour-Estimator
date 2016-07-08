8519bdce0f8d22d4e47ffe8bba4c20258db2d821
Garri Adrian Nablo <garri.almighty@gmail.com>
2016-07-08 18:56:12 +0800

    Added RealmUtility

---
+++ Hours Estimator/RealmUtility.swift
@@ -0,0 +1,13 @@
+//
+//  RealmUtility.swift
+//  Hours Estimator
+//
+//  Created by Garri Adrian Nablo on 7/8/16.
+//  Copyright © 2016 Knack Studios. All rights reserved.
+//
+
+import UIKit
+
+class RealmUtility: NSObject {
+
+}
